3291fd23ed2a6c95e6e905ecff8af4fb10c92820
Brooke Heck <b19heck@gmail.com>
2022-07-18 09:43:15 -0700

    create indivual shop objects

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ class CookieShop {
   minCust;
   maxCust;
   average;
+  totalSales;
 
   constructor(location, minCust, maxCust, average) {
     this.location = location;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.average = average;
+    this.totalSales = this.getHourlyTotals();
   }
 
   
@@ -16,9 +18,8 @@ class CookieShop {
     let cookieTotals = [];
     let total = 0;
     for(let i = 0; i < 14; i++) {
-      let min = Math.ceil(this.average - 15);
-      let max = Math.floor(average + 15);
-      let hourly = Math.floor(Math.random() * (max - min) + min);
+      let hourly = Math.floor(Math.random() * this.average
+       + 2);
       cookieTotals.push(hourly);
       total += hourly;
     }
@@ -26,4 +27,12 @@ class CookieShop {
     return cookieTotals;
   }
 }
+
+const seattle = new CookieShop('Seattle', 23, 65, 6.3);
+const tokyo = new CookieShop('Tokyo', 3, 24, 1.2);
+const dubai = new CookieShop('Dubai', 11, 38, 3.7);
+const paris = new CookieShop('Paris', 20, 38, 2.3);
+const lima = new CookieShop('Lima', 2, 16, 4.6);
+
+
 
