5ff042feeadc9ebce6daecb334da1f6106059f9b
Brooke Heck <b19heck@gmail.com>
2022-07-18 11:14:15 -0700

    add data tables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class CookieShop {
   location;
   minCust;
@@ -33,6 +35,27 @@ const tokyo = new CookieShop('Tokyo', 3, 24, 1.2);
 const dubai = new CookieShop('Dubai', 11, 38, 3.7);
 const paris = new CookieShop('Paris', 20, 38, 2.3);
 const lima = new CookieShop('Lima', 2, 16, 4.6);
+
+const shopArr = [seattle, tokyo, dubai, paris, lima];
+const properties = ['location', 'minCust', 'maxCust', 'average', 'totalSales'];
+
+function createDisplayTable(shop) {
+  const list = document.createElement('ul');
+  list.innerHTML = shop.location;
+  for(let i = 1; i < 5; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${properties[i]}: ${shop[properties[i]]}`;
+    list.append(listItem);
+  }
+  return list;
+}
+
+const container = document.querySelector('#tables');
+for(let shop of shopArr) {
+  container.appendChild(createDisplayTable(shop));
+}
+
+
 
 
 
